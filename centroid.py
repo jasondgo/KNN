d7bb2a5175ea954ed945467657408d351989bde5
Jason <jasondgomez04@gmail.com>
2022-05-17 15:03:25 -0500

    Setup for k-clustering

---
+++ centroid.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Centroid:
+    flower: str;
+    slPosition: float;
+    swPosition: float;
+    plPosition: float;
+    pwPosition: float;
+    
---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+  print("This Worked");
+
+
+
+
+if __name__ == "__main__":
+  main();
+
+
+
